eadc292d556b0e9fb4f13a9c273c30a0bf745b48
sachi <ardyosa@hotmail.com>
2025-09-09 16:10:42 -0500

    Inclusión de array list

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+// A traves de array list
+        List<Animal> tusAnimales = new ArrayList<>();
+        tusAnimales.add(new Perro());
+        tusAnimales.add(new Perro());
+        tusAnimales.add(new Perro());
+        tusAnimales.add(new Perro());
+
+        for (Animal animal : tusAnimales) {
+             animal.hacerSonido();
+        }
+
+
+// A traves de arrays
         Animal[] misAnimales = new Animal[3];
-        
         misAnimales[0] = new Perro();
         misAnimales[1] = new Gato();
-        misAnimales[2] = new Animal(); 
+        misAnimales[2] = new Animal();
+
+       
 
         for (Animal cualquiera : misAnimales) {
             cualquiera.hacerSonido();
         }
 
     }
-    
-}
 
+}
